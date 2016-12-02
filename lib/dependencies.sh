0db6dd654fc31d8a263bec274796ca79e9ca0f49
kondurupurandhar <kondurupurandhar@gmail.com>
2016-12-02 12:40:56 +0530

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -10,6 +10,8 @@ install_node_modules() {
       echo "Installing node modules (package.json)"
     fi
     #npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    cd $build_dir && npm install
+    cd -
   else
     echo "Skipping (no package.json)"
   fi
